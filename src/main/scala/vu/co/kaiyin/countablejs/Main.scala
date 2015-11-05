524f9d15a9abe605cb387037fc4747c48ba1acb7
Kaiyin Zhong <kindlychung@gmail.com>
2015-11-05 18:15:40 +0100

    readme

--- src/main/scala/vu/co/kaiyin/countablejs/Main.scala
+++ src/main/scala/vu/co/kaiyin/countablejs/Main.scala
@@ -1,11 +1,8 @@
 package vu.co.kaiyin.countablejs
 
 import org.scalajs.dom
-import org.scalajs.dom.raw.Element
-import vu.co.kaiyin.sjsdp.domepk.Dome.{Dome, dome}
 
 import scala.language.implicitConversions
-import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExport
 
 /**
@@ -15,7 +12,7 @@ import scala.scalajs.js.annotation.JSExport
 object Main {
   @JSExport
   def main(): Unit = {
-    val mathp: Element = dom.document.getElementById("pick")
+    val mathp: dom.Element = dom.document.getElementById("pick")
     Countable.live(mathp, (e: dom.Element, res: CountResult) => {
       dom.document.getElementById("count").textContent = "%d, %d, %d, %d, %d".format(res.all, res.characters, res.paragraphs, res.sentences, res.words)
     })
